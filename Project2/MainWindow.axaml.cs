c7d39ee7e31bb2b953a770b80f97eed360dc8240
Tak1 <vovan.kop04@mail.ru>
2023-11-15 21:07:03 +0700

    new table

--- Project2/MainWindow.axaml.cs
+++ Project2/MainWindow.axaml.cs
@@ -1,11 +1,41 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
+using MySql.Data.MySqlClient;
+using Project2.Models;
 
 namespace Project2;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    public string _connString = "server=Project2;database = sql12661858; port=3306 User=sql12661858 password = NF2LEehAvp";
+    private List<Order> _orders;
+    private MySqlConnection _connection;
+    public void ShowTable()
     {
-        InitializeComponent();
+        string sql = "select ЗаказID, Заказчик, Автомобиль, Груз, Статус_заказа from sql12661858.Заказ" +
+            "join Заказчик Z on Order.Client = Z.Client"+
+            "join Груз Г on Order.Груз = Г.ГрузID"+
+            "join Автомобиль А on Order.Автомобиль = А.Auto"+
+            "join Статус_заказа Сз on Order.Статус_заказа = Сз.Статус_ID";
+        _orders = new List<Order>();
+        _connection = new MySqlConnection(_connString);
+        _connection.Open();
+        MySqlCommand command = new MySqlCommand(sql, _connection);
+        MySqlDataReader reader = command.ExecuteReader();
+        while (reader.Read() && reader.HasRows)
+        {
+            var currentOrder = new Order()
+            {
+                Order_id = reader.GetInt32(column: "Order_id"),
+                Client = reader.GetInt32(column:"Client"),
+                Auto = reader.GetInt32(column:"Auto"),
+                Cargo = reader.GetInt32(column:"Cargo"),
+                Status =reader.GetInt32(column:"Status")
+            };
+            _orders.Add(currentOrder);
+        }
+        _connection.Close();
+        OrderGrid.ItemsSource = _orders;
     }
+    
 }
---
+++ Project2/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Project2.Models;
+
+public class Order
+{
+    public int Order_id { get; set; }
+    public int Auto { get; set; }
+    public int Cargo { get; set; }
+    public int Status { get; set; }
+    public int Client { get; set; }
+}
